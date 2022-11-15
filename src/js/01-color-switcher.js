b891cd51407da1e58741ec500d190e98484ea84b
Oleh <97728959+OlehIshchenko13@users.noreply.github.com>
2022-11-15 15:27:26 +0200

    frst task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+
+  const refs = {
+    body: document.querySelector('body'),
+    buttonStart: document.querySelector('[data-start]'),
+    buttonStop: document.querySelector('[data-stop]')
+  }
+refs.buttonStart.addEventListener('click',onClickBtnStart)
+refs.buttonStop.addEventListener('click',onClickBtnStop)
+
+let timerId = null;
+function onClickBtnStart  (e) {
+    timerId = setInterval(()=>{
+    refs.body.style.backgroundColor = getRandomHexColor()
+    },1000)
+
+    refs.buttonStart.disabled = true;
+
+     
+}
+
+function onClickBtnStop(e) {
+  clearInterval(timerId)
+  refs.buttonStart.disabled = false;
+  
+}
